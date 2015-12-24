e72f1076473739f819cfe208feaadd1a376f8c21
Candace Williford <candaceaw10@gmail.com>
2015-12-24 17:26:29 -0500

    first commit

---
+++ CodeWars-IsThisATriangle/Triangle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars_IsThisATriangle
+{
+    public static class Triangle
+    {
+        public static bool IsTriangle(int a, int b, int c)
+        {
+            if ((a + b > c) && (b + c > a) && (a + c > b))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
+
+//Implement a method that accepts 3 integer values a, b, c. The method should return true if 
+//a triangle can be built with the sides of given length and false in any other case.
+
+//(In this case, all triangles must have surface greater than 0 to be accepted).
+//-----------------------------------------------------------------------------
+//The Triangle Inequality Theorem
+//  A + B > C
+//  B + C > A
+//  A + C > B
+//All 3 of these conditions must return true
---
+++ IsThisATriangle-Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeWars_IsThisATriangle.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void IsTriangle_ValidPostiveNumbers_ReturnsTrue()
+        {
+            Assert.IsTrue(Triangle.IsTriangle(5, 7, 10));
+        }
+    }
+}
